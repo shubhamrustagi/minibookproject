6a708d8da695862560b6d57c047c47cdc719ab0a
shubhamrustagi <rustagishubham2000@gmail.com>
2022-11-05 10:23:53 +0530

    login page completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 //components
 import Messenger from "./components/Messenger";
 
 function App() {
+  const clientId =
+    "913078823338-kpq023i230fb8pbobkb8ln3n666hm786.apps.googleusercontent.com";
+
   return (
-    <div>
+    <GoogleOAuthProvider clientId={clientId}>
       <Messenger />
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -1,21 +1,85 @@
-import { Dialog, Box } from "@mui/material";
+import { Dialog, Box, Typography, List, ListItem, styled } from "@mui/material";
 
+import { qrCodeImage } from "../../constants/data";
+
+import { GoogleLogin } from "@react-oauth/google";
+
+import jwt_decode from "jwt-decode";
+
+const Component = styled(Box)`
+  display: flex;
+`;
+
+const Container = styled(Box)`
+  padding: 56px 0 56px 56px;
+`;
+
+const QRCode = styled("img")({
+  height: 264,
+  width: 264,
+  margin: "50px 0 0 50px",
+});
+
+const Title = styled(Typography)`
+  font-size: 28px;
+  color: #41525d;
+  font-weight: 300;
+  font-family: inherit;
+  margin-bottom: 25px;
+`;
+const StyledList = styled(List)`
+  & > li {
+    padding: 0;
+    margin-top: 15px;
+    font-size: 18px;
+    line-height: 28px;
+    color: #3b4a54;
+  }
+`;
 const dialogStyle = {
   height: "96%",
   marginTop: "13.5%",
-  width: "55%",
+  width: "47%",
   maxWidth: "100%",
   maxHeight: "100%",
   boxShadow: "none",
-  overFlow: "none",
+  overFlow: "hidden",
 };
 const LoginDialog = () => {
+  const onLoginSuccess = (res) => {
+    const decoded = jwt_decode(res.credential);
+    console.log(decoded);
+  };
+
+  const onLoginError = (res) => {
+    console.log("Login Failed", res);
+  };
   return (
     <Dialog open={true} PaperProps={{ sx: dialogStyle }} hideBackdrop={true}>
-      <Box>
-        <Box></Box>
-        <Box></Box>
-      </Box>
+      <Component>
+        <Container>
+          <Title>To use WhatsApp on your computer:</Title>
+          <StyledList>
+            <ListItem>1. Open WhatsApp on your phone</ListItem>
+            <ListItem>2. Tap Menu Settings and select Whatsapp Web</ListItem>
+            <ListItem>
+              3. Point your phone to this screen to capture the code
+            </ListItem>
+          </StyledList>
+        </Container>
+        <Box style={{ position: "relative" }}>
+          <QRCode src={qrCodeImage} alt="qr code" />
+          <Box
+            style={{
+              position: "absolute",
+              top: "50%",
+              transform: "translateX(50%)",
+            }}
+          >
+            <GoogleLogin onSuccess={onLoginSuccess} onError={onLoginError} />
+          </Box>
+        </Box>
+      </Component>
     </Dialog>
   );
 };
