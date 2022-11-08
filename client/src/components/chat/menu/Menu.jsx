7cf00440071aaf637998d89ea1b824d497c97b6c
shubhamrustagi <rustagishubham2000@gmail.com>
2022-11-08 08:56:26 +0530

    left side id added

--- client/src/components/chat/menu/Menu.jsx
+++ client/src/components/chat/menu/Menu.jsx
@@ -2,12 +2,14 @@
 import { Box } from "@mui/system";
 import Header from "./Header";
 import Search from "./Search";
+import Conversations from "./Conversations";
 
 const Menu = () => {
   return (
     <Box>
       <Header />
       <Search />
+      <Conversations />
     </Box>
   );
 };
